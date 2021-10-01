05f8d57ab8213c5196afcffdb3db60c3b0027c53
urmanural <einar.solfjell@lyse.net>
2021-10-01 15:22:21 +0200

    implement ReceiveBlock uncle/main block handling; change from miner methods calling each other to Tick() calling methods based on return values

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ const (
 	BLOCK_REWARD           = 10
 	UNCLE_REWARD           = 0
 	UNCLE_INCLUSION_REWARD = 0
+	SELFISH_PUBLISH_DELAY  = 10
 )
 
 type Miner interface {
@@ -37,7 +38,9 @@ type HonestMiner struct {
 }
 
 type SelfishMiner struct {
-	miner Miner
+	miner          Miner
+	publishQueue   [][]*Block	//circular array queue
+	publishCounter int
 }
 
 type Block struct {
@@ -62,7 +65,8 @@ func NewMiner(name string, neighbors []*Miner, mining_power int) Miner {
 
 func NewSelfishMiner(name string, neighbors []*Miner, mining_power int) Miner {
 	miner := NewMiner(name, neighbors, mining_power)
-	return &SelfishMiner{miner: miner}
+	queue := [][]*Block{}
+	return &SelfishMiner{miner: miner, publishQueue: queue, publish_counter: 0}
 }
 
 //initializes new block with a given parent, list of uncles and a timestamp
@@ -76,35 +80,59 @@ func NewBlock(parent *Block, uncles []*Block, timestamp int) *Block {
 	}
 }
 
+//do things the miner should do each time step
+func (m *HonestMiner) Tick() {
+	blockFound := m.Mine()
+	if blockFound {
+		block := m.BlockFound()
+		m.AppendBlock(block)
+		m.PublishBlock(block)
+	}
+
+	return
+}
+
+func (s *SelfishMiner) Tick () {
+	blockFound := s.Mine()
+	if blockFound {
+		s.BlockFound()
+		s.AppendBlock()
+		//enqueue block for delayed publishing
+	}
+
+	return
+}
+
 //miner makes an attempt to mine a new block
 //new block contains fees based on time since last block in chain
 //  later: expand with more detailed transaction/fees handling
 //new block can reference one or more uncles, gaining extra rewards
 //when block is found, trigger BlockFound()
-func (m *HonestMiner) Mine(tot_power, timestamp int) {
+//tot_power and timestamp tracked in main()
+func (m *HonestMiner) Mine(totPower, timestamp int) bool {
 	odds := m.miningPower / tot_power * 0.01
 	if odds > rand.Float64() {
-		m.BlockFound(timestamp)
+		return true
 	}
-	return
+	return false
 }
 
 //wait x ticks before announcing
 func (s *SelfishMiner) Mine() {
-	s.miner.Mine()
+	return s.miner.Mine()
 }
 
-func (m *HonestMiner) BlockFound(timestamp int) {
+func (m *HonestMiner) BlockFound(timestamp int) *Block {
 	parent := m.GetLastBlock()
 	uncles := m.GetUncles()
 	block := NewBlock(parent,uncles,timestamp + rand.Intn(99))	//timestamp in steps of 100 -> rand up to 99
-	m.blockchain = append(m.blockchain, block)
-	m.PublishBlock(block)
-	return
+	//m.blockchain = append(m.blockchain, block)
+	//m.PublishBlock(block)
+	return block
 }
 
-func (s *SelfishMiner) BlockFound() {
-	return
+func (s *SelfishMiner) BlockFound(timestamp int) {
+	return s.miner.BlockFound(timestamp)
 }
 
 func (m *HonestMiner) PublishBlock(b *Block) {
@@ -119,6 +147,20 @@ func (s *SelfishMiner) PublishBlock(b *Block) {
 }
 
 func (m *HonestMiner) ReceiveBlock(b *Block) {
+	last_block := m.GetLastBlock()
+	if b.depth > last_block.depth {
+		//append new block to chain
+		m.AppendBlock(b)
+	} else if b.depth < last_block.depth {
+		//append new block to uncles
+		m.AppendUncle(b)
+	} else if b.timestamp > last_block.timestamp {
+		//same depth -> move block with lower timestamp to chain and block with higher timestamp to uncles
+		m.AppendUncle(b)
+	} else {
+		m.uncles = append(m.uncles, m.PopBlock())
+		m.AppendBlock(b)
+	}
 	return
 }
 
@@ -143,6 +185,35 @@ func (s *SelfishMiner) GetLastBlock() *Block {
 	return s.miner.GetLastBlock()
 }
 
+func (m *HonestMiner) PopBlock() *Block {
+	chain := m.GetBlockChain()
+	m.blockchain, block = m.blockchain[:len(chain)-1], b.blockchain[len(chain)-1]
+	return block
+}
+
+func (s *SelfishMiner) PopBlock() *Block {
+	return s.miner.PopBlock()
+}
+
+func (m *HonestMiner) AppendBlock(b *Block) {
+	m.blockchain = append(m.blockchain, b)
+	return
+}
+
+func (s *SelfishMiner) AppendBlock(b *Block) {
+	s.miner.AppendBlock(b)
+	return
+}
+
+func (m *HonestMiner) AppendUncle(b *Block) {
+	m.uncles = append(m.uncles, b)
+	return
+}
+
+func (s *SelfishMiner) AppendUncle(b *Block) {
+	s.miner.AppendUncle(b)
+	return
+}
 func (m *HonestMiner) GetUncles() []*Block {
 	return m.uncles
 }
